0496cd1759c8a583490d1c3ac8401df7d7e7c315
suzuki.sh <s2terminal@users.noreply.github.com>
2020-04-11 20:46:33 +0900

    時報の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
 import * as React from "react";
 import "./styles.css";
+const MINUTE = 60
+const INTERVAL_SEC = MINUTE * 30;
+
+function say(text: string) {
+  const utter = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(utter);
+  console.log(text);
+}
+
+function sayDate(date: Date) {
+  say(`現在${date.getHours()}時${date.getMinutes()}分です`);
+}
 
 export default function App() {
+  const [now, setNow] = React.useState(new Date());
+  const [nextSayAt, setNextSayAt] = React.useState(0);
+  const nowDaySec = (((now.getHours() * 60) + now.getMinutes()) * MINUTE) + now.getSeconds();
+
+  // 1秒に1回の更新
+  React.useEffect(() => {
+    const timerID = setInterval(() => { setNow(new Date()); }, 1000);
+    return () => { clearInterval(timerID); };
+  });
+
+  // 発言
+  React.useEffect(() => {
+    function resetNextSayAt(daySec: number) {
+      let next: number = Math.floor((daySec + INTERVAL_SEC) / INTERVAL_SEC) * INTERVAL_SEC;
+      if (next <= nowDaySec) { next = resetNextSayAt(next); }
+      return next;
+    }
+
+    if (nowDaySec > nextSayAt) {
+      sayDate(now);
+      setNextSayAt(resetNextSayAt(nowDaySec));
+    }
+  }, [nowDaySec, nextSayAt, now]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>時報</h1>
+      <p>このタブを開いておくと、{INTERVAL_SEC/MINUTE}分おきに現在時刻をお伝えします。</p>
+      <p>次の時報は{Math.round((nextSayAt - nowDaySec)/MINUTE)}分後にお伝えします。</p>
+      <button onClick={() => sayDate(now)}>いま何時？</button>
     </div>
   );
 }
